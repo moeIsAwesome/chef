4172a949da8f40f1ee997effacff51a90cb0f074
Moe <musavimoe@gmail.com>
2024-03-15 20:43:16 +0100

    ✨ feat(pizza-dough): add bear dough calculator

---
+++ src/components/CookingCheckbox.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Checkbox, Label } from 'flowbite-svelte';
+	export let instruction: string;
+	export let name: string;
+	export let isDisabled: boolean = true;
+
+	export let removeDisableFromTheOther: () => void;
+</script>
+
+<div class="flex justify-between border-b-2 py-1">
+	<Label class="px-2" for={name}>{instruction}</Label>
+	<Checkbox id={name} bind:disabled={isDisabled} on:change={removeDisableFromTheOther} />
+</div>
+<!-- add isDisabled conditionally -->
